655857a1b29c669bcbe61e47dadfa6be4f012a55
Daniel Schmidt <d.schmidt@uni-jena.de>
2016-02-02 16:59:02 +0100

    Initialized git setup with Eclipse and "Hello World" program.

---
+++ FermiOwn.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : FermiOwn.cpp
+// Author      : Daniel Schmidt
+// Version     :
+// Copyright   : 
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Welcome to my own world!");
+	return EXIT_SUCCESS;
+}
