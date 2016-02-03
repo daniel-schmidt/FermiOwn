c3e9991b1936fa070103e26c34e9a8a67adf6c08
Daniel Schmidt <d.schmidt@uni-jena.de>
2016-02-03 18:00:53 +0100

    Operators implemented for FieldScalar.

--- FieldScalar.h
+++ FieldScalar.h
@@ -25,12 +25,23 @@ public:
 	FieldScalar( const Lattice & newLat, InitType init );
 	virtual ~FieldScalar();
 
+	FieldScalar<ScalarType>& operator+=( const FieldScalar<ScalarType>& rhs );
+	FieldScalar<ScalarType>& operator-=( const FieldScalar<ScalarType>& rhs );
+	FieldScalar<ScalarType>& operator*=( const ScalarType& rhs );
+	FieldScalar<ScalarType>& operator/=( const ScalarType& rhs );
+
+	template<class T> friend T operator*( FieldScalar<T> lhs, FieldScalar<T> rhs);
+
 	void Print();
 
 private:
 	Eigen::Matrix< ScalarType, Eigen::Dynamic, 1> data;
 	const Lattice & lat;
 };
+
+/* ---------------------------------------------------------------------------------------------------
+ * Implementation of member functions
+ * ---------------------------------------------------------------------------------------------------*/
 
 template<class ScalarType> FieldScalar<ScalarType>::FieldScalar(const Lattice& newLat, InitType init) :
 lat(newLat)
@@ -52,8 +63,57 @@ lat(newLat)
 
 template<class ScalarType> FieldScalar<ScalarType>::~FieldScalar() {}
 
+template<class ScalarType> FieldScalar<ScalarType>& FieldScalar<ScalarType>::operator+=( const FieldScalar<ScalarType>& rhs ) {
+	data += rhs.data;
+	return *this;
+}
+
+template<class ScalarType> FieldScalar<ScalarType>& FieldScalar<ScalarType>::operator-=( const FieldScalar<ScalarType>& rhs ) {
+	data -= rhs.data;
+	return *this;
+}
+
+template<class ScalarType> FieldScalar<ScalarType>& FieldScalar<ScalarType>::operator*=( const ScalarType& rhs ) {
+	data *= rhs;
+	return *this;
+}
+
+template<class ScalarType> FieldScalar<ScalarType>& FieldScalar<ScalarType>::operator/=( const ScalarType& rhs ) {
+	data /= rhs;
+	return *this;
+}
+
 template<class ScalarType> void FieldScalar<ScalarType>::Print() {
 	std::cout << data << std::endl;
+}
+
+/* ---------------------------------------------------------------------------------------------------
+ * Implementation of non-member functions
+ * ---------------------------------------------------------------------------------------------------*/
+
+template<class ScalarType> inline FieldScalar<ScalarType> operator+( FieldScalar<ScalarType> lhs, const FieldScalar<ScalarType>& rhs ) {
+	lhs += rhs;
+	return lhs;
+}
+
+template<class ScalarType> inline FieldScalar<ScalarType> operator-( FieldScalar<ScalarType> lhs, const FieldScalar<ScalarType>& rhs ) {
+	lhs -= rhs;
+	return lhs;
+}
+
+template<class ScalarType> FieldScalar<ScalarType>& operator/( FieldScalar<ScalarType> lhs, const ScalarType& rhs ) {
+	lhs /= rhs;
+	return lhs;
+}
+
+template<class ScalarType> FieldScalar<ScalarType>& operator*( FieldScalar<ScalarType> lhs, const ScalarType& rhs ) {
+	lhs *= rhs;
+	return lhs;
+}
+
+/* Point-wise product of fields */
+template<class ScalarType> ScalarType operator*( FieldScalar<ScalarType> lhs, FieldScalar<ScalarType> rhs) {
+	return lhs.data.dot(rhs.data);
 }
 
 #endif /* FIELDSCALAR_H_ */
--- FieldScalar_test.cpp
+++ FieldScalar_test.cpp
@@ -9,7 +9,7 @@
 #include "Lattice.h"
 
 int main() {
-	size_t Nt = 3, Ns = 2, dim = 2;
+	size_t Nt = 2, Ns = 2, dim = 3;
 	Lattice lat(Nt, Ns, dim);
 
 	FieldScalar<Real> fs0(lat, zeroInit);
@@ -18,5 +18,24 @@ int main() {
 	fs1.Print();
 	FieldScalar<Complex> fsrnd(lat, randomInit);
     fsrnd.Print();
+
+    std::cout << std::endl << "Trying +=" << std::endl;
+    fs1 += fsrnd;
+    fs1.Print();
+
+    std::cout << std::endl << "Trying +" << std::endl;
+    (fs1 + fsrnd).Print();
+
+    FieldScalar<Complex> fsmult(lat, oneInit);
+    std::cout << std::endl << "Trying *=Scalar" << std::endl;
+    fsmult *= Complex(0.,1.);
+    fsmult.Print();
+    fsmult /= Complex(0.,1.);
+    fsmult.Print();
+
+    FieldScalar<Complex> fsmult2(lat, oneInit);
+    fsmult2 *= 2.;
+    fsmult *= 3.;
+    std::cout<< "Result of scalar product is " << (fsmult*fsmult2) << " and should be (48,0)."<< std::endl;
 
 }
