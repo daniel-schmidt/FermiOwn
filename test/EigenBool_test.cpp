add867fb1f166ed05eb37c25db719c7fea796b91
Daniel Schmidt <d.schmidt@uni-jena.de>
2016-03-15 11:19:00 +0100

    Test of Boolean Vectors in Eigen added, to be used for the new model.

---
+++ test/EigenBool_test.cpp
@@ -0,0 +1,43 @@
+/*
+ * EigenBool_test.cpp
+ *
+ *  Created on: 15.03.2016
+ *      Author: dschmidt
+ */
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main( int argc, char** argv ) {
+	if( argc != 2 ) {
+		std::cerr << "Need 1 argument: vector size" << std::endl;
+		exit(1);
+	}
+
+	size_t N = atoi( argv[1]);
+
+	using namespace Eigen;
+
+	typedef Matrix< bool, 1, Dynamic > VectorXb;
+
+	VectorXb vecTrue = VectorXb::Constant(N,true);
+	std::cout << vecTrue << std::endl;
+
+	VectorXb vecFalse = VectorXb::Constant(N,false);
+	std::cout << vecFalse << std::endl;
+
+//	std::cout << vecTrue*vecFalse << std::endl;
+//	std::cout << vecTrue*vecTrue << std::endl;
+//	std::cout << vecFalse*vecFalse << std::endl;
+	std::cout << vecFalse.dot(vecTrue) << std::endl;
+	std::cout << vecTrue.dot(vecTrue) << std::endl;
+
+	std::cout << vecTrue+vecFalse << std::endl;
+	std::cout << vecTrue+vecTrue << std::endl;
+	std::cout << vecFalse + vecFalse << std::endl;
+	std::cout << vecFalse - vecFalse << std::endl;
+	std::cout << vecTrue - vecTrue << std::endl;
+	std::cout << vecTrue.count() << std::endl;
+	vecTrue(0) = false;
+	std::cout << vecTrue.count() << std::endl;
+}
