e3ce1a8caf1ab3d8c6a0cc2f47ee553ba98e80db
Daniel Schmidt <d.schmidt@uni-jena.de>
2016-03-17 11:00:20 +0100

    Works again for Nf=1, modified for larger Nf.

--- apps/FermiOwnGraph.cpp
+++ apps/FermiOwnGraph.cpp
@@ -26,15 +26,16 @@ Complex calcDet( SlacOperatorMatrix& dslac, const VectorXb& kx ) {
 	return pow(I,dslac.getMatrix().rows())*dslac.det();
 }
 
-Complex calcDet( std::vector<SlacOperatorMatrix>& dslac, const MatrixXb& kxab ) {
+Complex calcDet( std::vector<SlacOperatorMatrix> dslac, const MatrixXb& kxab ) {
 	Complex det = 1.;
-	for( int a = 0; a < kxab.cols(); a++ ) {
-		for( int x = 0; x < kxab.rows(); x++ ) {
-			if( kxab( a, x ) ) {
-				dslac[a].deletePoint(x);
-			}
-		}
-		det *= pow(I,dslac[a].getMatrix().rows())*dslac[a].det();
+	for( size_t a = 0; a < dslac.size(); a++ ) {
+//		for( int x = 0; x < kxab.rows(); x++ ) {
+//			if( kxab( x, a ) ) {
+//				dslac[a].deletePoint(x);
+//			}
+//		}
+//		det *= pow(I,dslac[a].getMatrix().rows())*dslac[a].det();
+		det *= calcDet( dslac[a], kxab.col(a) );
 	}
 	return det;
 }
@@ -58,28 +59,25 @@ int main( int argc, char** argv ) {
 
 	using namespace FermiOwn;
 
-	if( argc != 7 ) {
-		std::cerr << "Need 6 arguments: lattice size, numThermal, numMeasures, upPerMeasure, coupling, Nf" << std::endl;
+	if( argc != 8 ) {
+		std::cerr << "Need 7 arguments: Nt, Ns, numThermal, numMeasures, upPerMeasure, coupling, Nf" << std::endl;
 		exit(1);
 	}
 
 	// read parameters from console
-	size_t N = atoi( argv[1] );
-	size_t V = N*(N-1)*(N-1);
-	int numThermal = atoi( argv[2] );
-	int numMeasures = atoi( argv[3] );
-	int upPerMeasure = atoi( argv[4] );
 	size_t dim = 3;
-	double lambda = atof( argv[5] );
-	int Nf = atof( argv[6] );
+	size_t Nt = atoi( argv[1] );
+	size_t Ns = atoi( argv[2] );
+	size_t V = Nt*Ns*Ns;
+	int numThermal = atoi( argv[3] );
+	int numMeasures = atoi( argv[4] );
+	int upPerMeasure = atoi( argv[5] );
+	double lambda = atof( argv[6] );
+	int Nf = atof( argv[7] );
 
 	// initialize classes
-	Lattice lat( N, N-1, dim );
-	std::vector<SlacOperatorMatrix> slacNf;
-	for( int a = 0; a < Nf; a++) {
-		SlacOperatorMatrix dslac3d( N, N-1, dim );
-		slacNf.push_back(dslac3d);
-	}
+	Lattice lat( Nt, Ns, dim );
+	std::vector<SlacOperatorMatrix> slacNf( Nf, SlacOperatorMatrix( Nt, Ns, dim ) );
 
 	// initialize random number generator and distributions
 	std::ranlux48 gen;
@@ -92,15 +90,14 @@ int main( int argc, char** argv ) {
 		lambda += 0.1;
 		double kappa = 2./lambda;
 
-
-//		VectorXb kx = VectorXb::Constant( V, true );
 		// Data layout: first Nf columns hold kxaa for a=1,..,Nf, then kxab with a<b orderd (1,2), (1,3), (2,3), ...
+
+		//		std::cout << "Nf*(Nf+1)/2="<<Nf*(Nf+1)/2 << std::endl;
 		MatrixXb kxab = MatrixXb::Constant( V, Nf*(Nf+1)/2, true );
 
 		double av_k = 0.;
 		double accrate = 0;
-		Complex detOld = calcDet(slacNf[0], kxab);
-		int count = 0;
+		Complex detOld = calcDet(slacNf, kxab);
 
 		for( int measure = 0; measure < numMeasures+numThermal; measure++) {
 			for( int i = 0; i < upPerMeasure; i++ ) {
@@ -111,46 +108,53 @@ int main( int argc, char** argv ) {
 				int b = intNf_dist(gen);
 				int y = lat.getNeighbours(x)[mu];
 
-				// calculate ks
-				VectorXb kx = kxab.leftCols(Nf).rowwise().count();
-				VectorXb ktx = kxab.rightCols( Nf*(Nf-1)/2 ).rowwise().count();
-
-				int dk = 0;		// change in kxaa
-				int dkt = 0;	// change in kxab
+				double factor = 1.;
+				int dk = 0;		// change in kxab
 
 				if( a == b ) {
 					// switching link with same flavour
-					kxab(a,x)
-					dk = -kx.count();
-					kx(x) = !kx(x);
-					kx(y) = !kx(y);
-					int k = kx.count();
-					dk += k;
-
+					dk = -kxab.leftCols(Nf).count();
+					kxab(x,linIndex(Nf,a, b)) = !kxab(x,linIndex(Nf,a, b));
+					kxab(y,linIndex(Nf,a, b)) = !kxab(y,linIndex(Nf,a, b));
+					dk += kxab.leftCols(Nf).count();
+					//					factor = Nf*Nf/(1.+Nf*Nf);
+					factor = -1.5;
 				} else {
-					// switching flavour at
+					// switching flavour at two positions
+					std::cout << "Thou shall not pass!" << std::endl;
+					dk = -kxab.rightCols( Nf*(Nf-1)/2 ).count();
+					kxab(x,linIndex(Nf,a, b)) = !kxab(x,linIndex(Nf,a, b));
+					kxab(y,linIndex(Nf,a, b)) = !kxab(y,linIndex(Nf,a, b));
+					dk += kxab.rightCols( Nf*(Nf-1)/2 ).count();
+					dk *= 2;
+					factor = sqrt(2);
 				}
 
-				Complex det = calcDet(slacNf[0], kx);
-//				std::cout << "x=" << x << "\ty=" << y << "\tk=" << k << "\tdk=" << dk << "\tdet: " << detOld-det;
-				double dw = std::pow(-1.5*kappa, dk);
+				Complex det = calcDet(slacNf, kxab);
+
+				//				std::cout << kxab << std::endl << std::endl;
+				//				std::cout << slacNf[0].getMatrix() << std::endl << std::endl;
+				//
+				//				std::cout << "x=" << x << "\ty=" << y << "\ta=" << a << "\tb=" << b;
+				//				std::cout << "\tka=" << kxab.leftCols(Nf).count() << "\tkab="<< kxab.rightCols( Nf*(Nf-1)/2 ).count() << "\tdk=" << dk;
+				//				std::cout << "\tdetOld: " << detOld << "\tdet: " << det;
+
+				double dw = std::pow(factor*kappa, dk);
 				Complex weight =  dw*(det/detOld);
 
 				double r = uni_real_dist(gen);
-
+				bool accepted = false;
 				if( std::fabs(weight) > r ) {
+					accepted = true;
 					accrate++;
 					detOld = det;
 				} else {
-					kx(x) = !kx(x);
-					kx(y) = !kx(y);
+					kxab(x,linIndex(Nf,a, b)) = !kxab(x,linIndex(Nf,a, b));
+					kxab(y,linIndex(Nf,a, b)) = !kxab(y,linIndex(Nf,a, b));
 				}
-//				std::cout << "\tdw: " << dw << "\tweight: " << weight << "\taccepted: " << accepted << std::endl;
-
-				count ++;
-				slacNf[0].setFull();
+				//				std::cout << "\tdw: " << dw << "\tweight: " << weight << "\taccepted: " << accepted << std::endl;
 			}
-			if( measure >= numThermal ) av_k += kx.count()/double(V);
+			if( measure >= numThermal ) av_k += ( kxab.leftCols(Nf).count()/*+2.*kxab.rightCols( Nf*(Nf-1)/2 ).count() */)/double(V);
 		}
 		av_k/=double(numMeasures);
 		accrate /= double((numMeasures+numThermal)*upPerMeasure);
