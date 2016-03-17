9005b2f483fd6fc07d0ef82a9c1b9487045b166c
Daniel Schmidt <d.schmidt@uni-jena.de>
2016-03-17 12:25:34 +0100

    Hypergeometric factor and calculation of na included.
    Nf=1 still working.

--- apps/FermiOwnGraph.cpp
+++ apps/FermiOwnGraph.cpp
@@ -14,7 +14,7 @@
 #include "Lattice.h"
 
 namespace FermiOwn {
-typedef Eigen::Matrix< bool, 1, Eigen::Dynamic > VectorXb;
+typedef Eigen::Matrix< bool, Eigen::Dynamic, 1 > VectorXb;
 typedef Eigen::Matrix< bool, Eigen::Dynamic, Eigen::Dynamic > MatrixXb;
 
 Complex calcDet( SlacOperatorMatrix& dslac, const VectorXb& kx ) {
@@ -29,12 +29,12 @@ Complex calcDet( SlacOperatorMatrix& dslac, const VectorXb& kx ) {
 Complex calcDet( std::vector<SlacOperatorMatrix> dslac, const MatrixXb& kxab ) {
 	Complex det = 1.;
 	for( size_t a = 0; a < dslac.size(); a++ ) {
-//		for( int x = 0; x < kxab.rows(); x++ ) {
-//			if( kxab( x, a ) ) {
-//				dslac[a].deletePoint(x);
-//			}
-//		}
-//		det *= pow(I,dslac[a].getMatrix().rows())*dslac[a].det();
+		//		for( int x = 0; x < kxab.rows(); x++ ) {
+		//			if( kxab( x, a ) ) {
+		//				dslac[a].deletePoint(x);
+		//			}
+		//		}
+		//		det *= pow(I,dslac[a].getMatrix().rows())*dslac[a].det();
 		det *= calcDet( dslac[a], kxab.col(a) );
 	}
 	return det;
@@ -50,6 +50,20 @@ int linIndex(int N, int i, int j) {
 			i = tmp;
 		}
 		return N+N*(N-1)/2 - (N-i)*(N-i-1)/2+j-i-1;
+	}
+}
+
+double getHypergeometricFactor( int flavour ) {
+	switch( flavour ){
+	case 0: return 1.;
+	case 1: return 1.5;
+	case 2: return 2.75;
+	case 3: return 53./8.;
+	case 4: return 345./16.;
+	case 5: return 2947./32.;
+	case 6: return 31411./64.;
+	default: std::cout << "Value of the confluent hypergeometric function for a=" << flavour << " not implemented!" << std::endl;
+	return -1.;
 	}
 }
 
@@ -111,50 +125,60 @@ int main( int argc, char** argv ) {
 				double factor = 1.;
 				int dk = 0;		// change in kxab
 
+				Eigen::VectorXi kx = kxab.leftCols(Nf).rowwise().count().cast<int>();
+				Eigen::VectorXi na(Nf+1);
+				for( int n = 0; n <= Nf; n++ ) {
+					na(n) = -(kx.array() == n).count();
+				}
+
 				if( a == b ) {
 					// switching link with same flavour
 					dk = -kxab.leftCols(Nf).count();
 					kxab(x,linIndex(Nf,a, b)) = !kxab(x,linIndex(Nf,a, b));
 					kxab(y,linIndex(Nf,a, b)) = !kxab(y,linIndex(Nf,a, b));
 					dk += kxab.leftCols(Nf).count();
-					//					factor = Nf*Nf/(1.+Nf*Nf);
-					factor = -1.5;
 				} else {
 					// switching flavour at two positions
-					std::cout << "Thou shall not pass!" << std::endl;
 					dk = -kxab.rightCols( Nf*(Nf-1)/2 ).count();
 					kxab(x,linIndex(Nf,a, b)) = !kxab(x,linIndex(Nf,a, b));
 					kxab(y,linIndex(Nf,a, b)) = !kxab(y,linIndex(Nf,a, b));
 					dk += kxab.rightCols( Nf*(Nf-1)/2 ).count();
 					dk *= 2;
-					factor = sqrt(2);
+					//					factor = sqrt(2);
 				}
 
 				Complex det = calcDet(slacNf, kxab);
 
-				//				std::cout << kxab << std::endl << std::endl;
-				//				std::cout << slacNf[0].getMatrix() << std::endl << std::endl;
-				//
-				//				std::cout << "x=" << x << "\ty=" << y << "\ta=" << a << "\tb=" << b;
-				//				std::cout << "\tka=" << kxab.leftCols(Nf).count() << "\tkab="<< kxab.rightCols( Nf*(Nf-1)/2 ).count() << "\tdk=" << dk;
-				//				std::cout << "\tdetOld: " << detOld << "\tdet: " << det;
+				kx = kxab.leftCols(Nf).rowwise().count().cast<int>();
+				for( int n = 0; n <= Nf; n++ ) {
+					na(n) += (kx.array() == n).count();
+					factor *= std::pow( getHypergeometricFactor( n ), double(na(n)) );
+//					std::cout << "n=" << n << "\tfactor=" << factor << "\thyperFactor=" << getHypergeometricFactor(n) << std::endl;
+				}
 
-				double dw = std::pow(factor*kappa, dk);
-				Complex weight =  dw*(det/detOld);
+				//				std::cout << slacNf[0].getMatrix() << std::endl << std::endl;
+//								std::cout << "kxab=" << std::endl << kxab << std::endl << std::endl;
+//								std::cout << "kx=" << std::endl << kx << std::endl;
+//								std::cout << "na=" << std::endl << na << std::endl;
+//								std::cout << "x=" << x << "\ty=" << y << "\ta=" << a << "\tb=" << b;
+//								std::cout << "\tka=" << kxab.leftCols(Nf).count() << "\tkab="<< kxab.rightCols( Nf*(Nf-1)/2 ).count() << "\tdk=" << dk;
+//								std::cout << "\tdetOld: " << detOld << "\tdet: " << det;
+				double dw = std::pow(kappa, dk);
+				Complex weight =  factor*dw*(det/detOld);
 
 				double r = uni_real_dist(gen);
-				bool accepted = false;
+//				bool accepted = false;
 				if( std::fabs(weight) > r ) {
-					accepted = true;
+//					accepted = true;
 					accrate++;
 					detOld = det;
 				} else {
 					kxab(x,linIndex(Nf,a, b)) = !kxab(x,linIndex(Nf,a, b));
 					kxab(y,linIndex(Nf,a, b)) = !kxab(y,linIndex(Nf,a, b));
 				}
-				//				std::cout << "\tdw: " << dw << "\tweight: " << weight << "\taccepted: " << accepted << std::endl;
+//				std::cout << "\tdw: " << dw << "\tweight: " << weight << "\taccepted: " << accepted << std::endl;
 			}
-			if( measure >= numThermal ) av_k += ( kxab.leftCols(Nf).count()/*+2.*kxab.rightCols( Nf*(Nf-1)/2 ).count() */)/double(V);
+			if( measure >= numThermal ) av_k += ( kxab.leftCols(Nf).count()+2.*kxab.rightCols( Nf*(Nf-1)/2 ).count() )/double(V);
 		}
 		av_k/=double(numMeasures);
 		accrate /= double((numMeasures+numThermal)*upPerMeasure);
