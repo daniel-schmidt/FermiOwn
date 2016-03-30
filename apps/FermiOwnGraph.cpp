60303adb9068e85c13ab0e17837c94aa3d2a8098
Daniel Schmidt <d.schmidt@uni-jena.de>
2016-03-30 16:54:38 +0200

    Basic changes to new boolean field, not working.

--- apps/FermiOwnGraph.cpp
+++ apps/FermiOwnGraph.cpp
@@ -10,51 +10,40 @@
 #include <random>
 #include <vector>
 #include <Eigen/Dense>
-#include "SlacOperatorMatrix.h"
 #include "Lattice.h"
+#include "FieldBoolean.h"
+#include "SlacOperatorMatrix.h"
 
 namespace FermiOwn {
 
-Complex calcDet( SlacOperatorMatrix& dslac, const VectorXb& kx ) {
-	for( int x = 0; x < kx.size(); x++ ) {
-		if( kx(x) ) {
-			dslac.deletePoint(x);
-		}
-	}
-	return pow(I,dslac.getMatrix().rows())*dslac.det();
-}
-int linIndex(int N, int i, int j) {
-	if( i == j) {
-		return i;
-	} else {
-		if( i > j ) {
-			int tmp = j;
-			j = i;
-			i = tmp;
-		}
-		return N+N*(N-1)/2 - (N-i)*(N-i-1)/2+j-i-1;
-	}
-}
-
-Complex calcDet( std::vector<SlacOperatorMatrix> dslac, const MatrixXb& kxab0, const MatrixXb& kxab1, int Nf ) {
-	Complex det = 1.;
-	for( size_t b = 0; b < Nf; b++ ) {
-		for( size_t a = 0; a < b; a++ ) {
-			VectorXb kx0 = kxab0.col( linIndex( Nf, a, b ) );
-			VectorXb kx1 = kxab1.col( linIndex( Nf, a, b ) );
-			dslac[a].eraseCols( kx0, kx1 );
-			dslac[b].eraseRows( kx0, kx1 );
-		}
-	}
-	for( size_t a = 0; a < Nf; a++ ) {
-		VectorXb kx0 = kxab0.col( a );
-		VectorXb kx1 = kxab1.col( a );
-		dslac[a].eraseCols( kx0, kx1 );
-		dslac[a].eraseRows( kx0, kx1 );
-		det *= pow(I,dslac[a].getMatrix().rows())*dslac[a].det();
-	}
-	return det;
-}
+//Complex calcDet( SlacOperatorMatrix& dslac, const VectorXb& kx ) {
+//	for( int x = 0; x < kx.size(); x++ ) {
+//		if( kx(x) ) {
+//			dslac.deletePoint(x);
+//		}
+//	}
+//	return pow(I,dslac.getMatrix().rows())*dslac.det();
+//}
+//
+//Complex calcDet( std::vector<SlacOperatorMatrix> dslac, const MatrixXb& kxab0, const MatrixXb& kxab1, int Nf ) {
+//	Complex det = 1.;
+//	for( size_t b = 0; b < Nf; b++ ) {
+//		for( size_t a = 0; a < b; a++ ) {
+//			VectorXb kx0 = kxab0.col( linIndex( Nf, a, b ) );
+//			VectorXb kx1 = kxab1.col( linIndex( Nf, a, b ) );
+//			dslac[a].eraseCols( kx0, kx1 );
+//			dslac[b].eraseRows( kx0, kx1 );
+//		}
+//	}
+//	for( size_t a = 0; a < Nf; a++ ) {
+//		VectorXb kx0 = kxab0.col( a );
+//		VectorXb kx1 = kxab1.col( a );
+//		dslac[a].eraseCols( kx0, kx1 );
+//		dslac[a].eraseRows( kx0, kx1 );
+//		det *= pow(I,dslac[a].getMatrix().rows())*dslac[a].det();
+//	}
+//	return det;
+//}
 
 
 double getHypergeometricFactor( int flavour ) {
@@ -71,19 +60,6 @@ double getHypergeometricFactor( int flavour ) {
 	}
 }
 
-bool constraintViolated( const MatrixXb& kxab0, const MatrixXb& kxab1, int Nf, int x ) {
-	bool violated = false;
-	for( int m = 0; m < Nf; m++ ) {
-		int constrSum = 0;
-		for( int n = 0; n < Nf; n++ )
-		{
-			constrSum += kxab0( x, linIndex( Nf, m, n ) ) + kxab1( x, linIndex( Nf, m, n ) );
-		}
-		//		std::cout << "constraint for a=" << m << " is " << constrSum << std::endl;
-		if( constrSum > 1 ) violated = true;
-	}
-	return violated;
-}
 }
 
 int main( int argc, char** argv ) {
@@ -105,10 +81,10 @@ int main( int argc, char** argv ) {
 	int upPerMeasure = atoi( argv[5] );
 	double lambda = atof( argv[6] );
 	int Nf = atof( argv[7] );
-
+	int numSpin = 2;
 	// initialize classes
 	Lattice lat( Nt, Ns, dim );
-	std::vector<SlacOperatorMatrix> slacNf( Nf, SlacOperatorMatrix( Nt, Ns, dim ) );
+	SlacOperatorMatrix slac( Nf, Ns, dim, Nf );
 
 	// initialize random number generator and distributions
 	std::ranlux48 gen;
@@ -124,19 +100,15 @@ int main( int argc, char** argv ) {
 
 		// Data layout: first Nf columns hold kxaa for a=1,..,Nf, then kxab with a<b orderd (1,2), (1,3), (2,3), ...
 		// initialize single-flavour part to true, the rest to false
-		std::vector<MatrixXb> kxab;
-		for( int spin = 0; spin <=1 ; spin++ ) {
-			kxab.push_back(MatrixXb::Constant( V, Nf*(Nf+1)/2, false ));
-			kxab.back().leftCols(Nf) = MatrixXb::Constant( V, Nf, true);
-		}
+		FieldBoolean kxab( lat.getVol(), numSpin, Nf, &gen, zeroInit );
 
 		double av_k = 0.;
 		double accrate = 0;
-		Complex detOld = calcDet(slacNf, kxab[0], kxab[1], Nf);
+		Complex detOld = slac.det();
 
 		for( int measure = 0; measure < numMeasures+numThermal; measure++) {
 			for( int i = 0; i < upPerMeasure; i++ ) {
-				// draw random point and 2 flavours
+				// draw random point, spin and 2 flavours
 				int x = intV_dist(gen);
 				//				int mu = int2mu_dist(gen);
 				int a = intNf_dist(gen);
@@ -149,12 +121,12 @@ int main( int argc, char** argv ) {
 				//				int dkt = 0;	// change in tilde kxab
 				// calculate old value of na
 
-				Eigen::VectorXi kxSpinSum = kxab[0].leftCols(Nf).cast<int>() + kxab[1].leftCols(Nf).cast<int>();
-				int n1 = 0;
-				int n2 = 0;
-
-				n1 = -(kxSpinSum.array() == 1).count();
-				n2 = -(kxSpinSum.array() == 2).count();
+//				Eigen::VectorXi kxSpinSum = kxab[0].leftCols(Nf).cast<int>() + kxab[1].leftCols(Nf).cast<int>();
+//				int n1 = 0;
+//				int n2 = 0;
+//
+//				n1 = -(kxSpinSum.array() == 1).count();
+//				n2 = -(kxSpinSum.array() == 2).count();
 
 //				for( int n = 0; n <= Nf; n++ ) {
 //					na(n) = -(kx.array() == n).count();
@@ -165,24 +137,24 @@ int main( int argc, char** argv ) {
 				//			constraintViolated( kxab, Nf, y );
 				//			std::cout << "updating..." << std::endl;
 
-				std::vector<MatrixXb> kxabOld(kxab);
+				FieldBoolean kxabOld(kxab);
 
 				// update at the drawn index
-				dk = -kxab[0].count() - kxab[1].count();
-				kxab[spin]( x, linIndex( Nf, a, b ) ) = !kxab[spin]( x, linIndex( Nf, a, b ) );
-				dk += kxab[spin].count();
+				dk = -kxab.sumAll();
+				kxab.invert( x, spin, a, b );
+				dk += kxab.sumAll();
 
 				if( a == b ) {
 					// updating with same flavour,
 					// choose, if we update another spin (setting a kxaa=2) or another point (setting two kxaa=1, keeping n1 even)
 
 					if( 0.5 > uni_real_dist(gen) ) {
-						kxab[ 1-spin ]( x, a ) = !kxab[ 1-spin ]( x, a );
+						kxab.invert( x, 1-spin, a, a );
 					} else {
 						int mu = int2mu_dist(gen);
 						int y = lat.getNeighbours( x )[mu];
-						kxab[spin]( y, a ) = !kxab[spin]( y, a );
-						if( constraintViolated( kxab[0], kxab[1], Nf, x ) ) {
+						kxab.invert( y, spin, a, a );
+						if( kxab.constraintViolated( y ) ) {
 							kxab = kxabOld;
 //							std::cout << "violated! Reset and continue loop..." << std::endl;
 							continue;
@@ -190,22 +162,21 @@ int main( int argc, char** argv ) {
 					}
 				} else {
 					// updating with two different flavours, enforce kxab = kxba
+					int spin2 = intSpin_dist(gen);
 					if( dk < 0 ) {
 						// we have to delete another spin in kxba, choose one randomly and check,
 						// if it is set, otherwise the other one must be set, since we have only two spins.
-						int spin2 = intSpin_dist(gen);
-						if( kxab[spin2]( x, linIndex( Nf, b, a ) ) ) {
-							kxab[spin2]( x, linIndex( Nf, b, a ) ) = ! kxab[spin2]( x, linIndex( Nf, b, a ) );
+						if( kxab.getValue( x, spin2, b, a ) ) {
+							kxab.invert( x, spin2, b, a );
 						} else {
-							kxab[1-spin2]( x, linIndex( Nf, b, a ) ) = ! kxab[1-spin2]( x, linIndex( Nf, b, a ) );
+							kxab.invert( x, 1-spin2, b, a );
 						}
 					} else {
 						// we have to set another spin in kxba as before
-						int spin2 = intSpin_dist(gen);
-						if( !kxab[spin2]( x, linIndex( Nf, b, a ) ) ) {
-							kxab[spin2]( x, linIndex( Nf, b, a ) ) = ! kxab[spin2]( x, linIndex( Nf, b, a ) );
+						if( !kxab.getValue( x, spin2, b, a ) ) {
+							kxab.invert( x, spin2, b, a );
 						} else {
-							kxab[1-spin2]( x, linIndex( Nf, b, a ) ) = ! kxab[1-spin2]( x, linIndex( Nf, b, a ) );
+							kxab.invert( x, 1-spin2, b, a );
 						}
 					}
 
@@ -219,14 +190,15 @@ int main( int argc, char** argv ) {
 					//					}
 					//					factor *= pow( 2., double(dk) );
 				}
-				dk += kxab[0].count() + kxab[1].count();
+				dk += kxab.sumAll();
 
-				if( constraintViolated( kxab[0], kxab[1], Nf, x ) ) {
+				if( kxab.constraintViolated( x ) ) {
 					kxab = kxabOld;
 					//				std::cout << "violated! Reset and continue loop..." << std::endl;
 					continue;
 				}
-				Complex det = calcDet(slacNf, kxab[0], kxab[1], Nf);
+				slac.erase( kxab );
+				Complex det = slac.det();
 
 				// TODO: calculate change in na
 
@@ -263,7 +235,7 @@ int main( int argc, char** argv ) {
 				}
 				//			std::cout << "\tdw: " << dw << "\tweight: " << weight << "\taccepted: " << accepted << std::endl;
 			}
-			if( measure >= numThermal ) av_k += ( kxab[0].count() + kxab[1].count() )/double(V);
+			if( measure >= numThermal ) av_k += kxab.sumAll()/double(V);
 		}
 		av_k/=double(numMeasures);
 		accrate /= double((numMeasures+numThermal)*upPerMeasure);
