8b88b81e7b2ab9658df3913d1c42e71abdee260b
Daniel Schmidt <d.schmidt@uni-jena.de>
2016-06-21 11:43:56 +0200

    ExactPartitionSum updated, works fine.

--- apps/ExactPartitionSum.cpp
+++ apps/ExactPartitionSum.cpp
@@ -5,6 +5,7 @@
  *      Author: dschmidt
  */
 
+#include <iostream>
 #include <Eigen/Dense>
 #include "FieldBoolean.h"
 #include "ConfigGenerator.h"
@@ -14,52 +15,62 @@ int main( int argc, char** argv ) {
 
 	using namespace FermiOwn;
 
-	size_t latVol = 4;
+	size_t Nt = 4;
+	size_t Ns = 1;
+	size_t dim = 3;
+	size_t latVol = Nt*Ns*Ns;
 	size_t numSpins = 2;
-	size_t numFlavours = 2;
+	size_t Nf = 2;
 
-	FieldBoolean kxiab( latVol, numSpins, numFlavours, NULL, zeroInit );
-	ConfigGenerator confGen( numSpins, numFlavours );
-	WeightFunction weight( kxiab );
+	std::ofstream fout( "exactPartitionSum.dat");
 
-	confGen.generateAllowedConfs();
-	Eigen::MatrixXi allowedConfs = confGen.getAllConfs();
+	double dlambda = 0.1;
+	for( int i = 1; i <= 20; i++ ) {
+		double lambda = dlambda*i;
+		FieldBoolean kxiab( latVol, numSpins, Nf, NULL, zeroInit );
+		ConfigGenerator confGen( numSpins, Nf );
+		WeightFunction weight( kxiab, Nt, Ns, dim, Nf, lambda );
 
-	size_t confsPerX = allowedConfs.rows();
-	//TODO: this should check somehow, if all configs fit in memory...
-	size_t numConfigs = std::pow( confsPerX, latVol );
-//	std::cout << "numConfigs: " << numConfigs << std::endl;
+		confGen.generateAllowedConfs();
+		MatrixXb allowedConfs = confGen.getAllConfs();
 
-	Complex sum = 0.;
+		size_t confsPerX = allowedConfs.rows();
+		//TODO: this should check somehow, if all configs fit in memory...
+		size_t numConfigs = std::pow( confsPerX, latVol );
+		//	std::cout << "numConfigs: " << numConfigs << std::endl;
 
-	for( size_t conf = 0; conf < numConfigs; conf++ ) {
-		size_t confNum = conf;
-//		std::cout << "Conf " << conf << " confNum: ";
+		Complex sum = 0.;
 
-		Eigen::VectorXi confList = Eigen::VectorXi::Zero( latVol );
-		size_t count = 0;
-		while( confNum >= confsPerX ) {
-			confList( count ) = confNum % confsPerX;
-			confNum /= confsPerX;
-			count ++;
-		}
+		for( size_t conf = 0; conf < numConfigs; conf++ ) {
+			size_t confNum = conf;
+			//		std::cout << "Conf " << conf << " confNum: ";
 
-		confList( count ) = confNum;
-//		std::cout << std::endl<< confList <<std::endl;
+			Eigen::VectorXi confList = Eigen::VectorXi::Zero( latVol );
+			size_t count = 0;
+			while( confNum >= confsPerX ) {
+				confList( count ) = confNum % confsPerX;
+				confNum /= confsPerX;
+				count ++;
+			}
 
-		for( size_t x = 0; x < latVol; x++ ) {
-			size_t newConfIndex = confList( x );
-			for( size_t spin = 0; spin < 2; spin++ ) {
-				for( size_t a = 0; a < numFlavours; a++ ) {
-					for( size_t b = 0; b < numFlavours; b++ ) {
-						kxiab.setValue( bool(allowedConfs( newConfIndex, spin*numFlavours*numFlavours + b*numFlavours + a)), x, spin, a, b );
+			confList( count ) = confNum;
+			//		std::cout << std::endl<< confList <<std::endl;
+
+			for( size_t x = 0; x < latVol; x++ ) {
+				size_t newConfIndex = confList( x );
+				for( size_t spin = 0; spin < 2; spin++ ) {
+					for( size_t a = 0; a < Nf; a++ ) {
+						for( size_t b = 0; b < Nf; b++ ) {
+							kxiab.setValue( bool(allowedConfs( newConfIndex, spin*Nf*Nf + b*Nf + a)), x, spin, a, b );
+						}
 					}
 				}
 			}
-		}
-//		kxiab.Print();
+			//		kxiab.Print();
 
-		sum += weight.calculateWeight();
+			sum += weight.calculateWeight();
+		}
+		fout << lambda << "\t" << sum << std::endl;
 	}
-	std::cout << 2./kappa << "\t" << sum << std::endl;
+	fout.close();
 }
